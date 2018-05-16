f0ae77f099f5e0ec77ef3b1bbd5d0b36a221937a
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-17 01:47:25 +0900

    マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#top'
   resources :posts, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show]
   get '/' => 'posts#top'
 end
