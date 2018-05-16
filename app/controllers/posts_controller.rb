d364846b0750e031c77a96c852beb0c220ac24e6
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-16 17:54:12 +0900

    投稿機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,28 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
-	def create
+	def top
+		
+	end
 
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		@post.save
+		redirect_to posts_path
 	end
 
 	def index
-		@users = User.all
+		@posts = Post.all
+		@post = Post.new
 	end
 
 	def show
 		
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :opinion, :user_id)
+		end
 
 end
