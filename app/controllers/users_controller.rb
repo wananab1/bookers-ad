b94f73e76592edad1dae5382947a8f0299af6158
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-17 15:53:26 +0900

    プロフィール編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@posts = @user.posts
   end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to user_path(@user.id)
+  end
+
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :intro)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#top'
   resources :posts, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show ,:edit, :update]
   get '/' => 'posts#top'
 end
