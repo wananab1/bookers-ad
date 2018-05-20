21bbc7c182275261f80777665347a9e55cec34f9
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-20 20:28:35 +0900

    MyPage.Users.Booksデザイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
+    @post = Post.new
   end
 
   def show
+    @post = Post.new
   	@user = User.find(params[:id])
   	@posts = @user.posts
   end
