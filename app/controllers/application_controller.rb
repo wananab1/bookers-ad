395ded66d9529d583a05cd439b6ebc6b464ca722
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-21 21:49:08 +0900

    Finish 5-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
 protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name ,:email, :password])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
 	end
 
 end
